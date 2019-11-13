82c41443b365f1a1293798a21fedd3d9d43e7b88
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-12 21:38:06 -0500

    built form for donor splash page

--- client/src/pages/Donor.js
+++ client/src/pages/Donor.js
@@ -2,25 +2,127 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../actions/authActions";
+import classnames from "classnames";
+
 class Dashboard extends Component {
+
+  // Define "submit" function
+  submitFood = () => {
+
+  };
+
+  // Button Functions
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
   };
-render() {
-    const { user } = this.props.auth;
 
-return (
+  onSubmitClick = e => {
+    e.preventDefault();
+    this.props.submitFood();
+  };
+
+render() {
+  const { user } = this.props.auth;
+  const { errors } = this.state;
+
+  return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.firstname}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged in as  {user.usertype}
-                <span style={{ fontFamily: "monospace" }}></span>
-              </p>
-            </h4>
+
+            {/* Form */}
+            <h4>Submit an Item!</h4>
+            <div className="input-field col s12">
+              <input
+                onChange={this.onChange}
+                value={this.state.itemname}
+                error={errors.itemname}
+                id="itemname"
+                type="text"
+                className={classnames("", {
+                  invalid: errors.itemname
+                })}
+              />
+              <label htmlFor="itemname">Item Name</label>
+              <span className="red-text">{errors.itemname}</span>
+            </div>
+
+            <div className="input-field col s12">
+              <input
+                onChange={this.onChange}
+                value={this.state.numberofunits}
+                error={errors.numberofunits}
+                id="numberofunits"
+                type="number"
+                className={classnames("", {
+                  invalid: errors.numberofunits
+                })}
+              />
+              <label htmlFor="numberofunits">Number of Units (Please input a number!)</label>
+              <span className="red-text">{errors.numberofunits}</span>
+            </div>
+
+            <div className="input-field col s12">
+              <input
+                onChange={this.onChange}
+                value={this.state.description}
+                error={errors.description}
+                id="description"
+                type="text"
+                className={classnames("", {
+                  invalid: errors.description
+                })}
+              />
+              <label htmlFor="description">Description</label>
+              <span className="red-text">{errors.description}</span>
+            </div>
+
+            <div className="input-field col s12">
+              <input
+                onChange={this.onChange}
+                value={this.state.shelflife}
+                error={errors.shelflife}
+                id="shelflife"
+                type="text"
+                className={classnames("", {
+                  invalid: errors.shelflife
+                })}
+              />
+              <label htmlFor="shelflife">Shelf Life</label>
+              <span className="red-text">{errors.shelflife}</span>
+            </div>
+
+            <div className="input-field col s12">
+              <input
+                onChange={this.onChange}
+                value={this.state.pickuptime}
+                error={errors.pickuptime}
+                id="pickuptime"
+                type="text"
+                className={classnames("", {
+                  invalid: errors.pickuptime
+                })}
+              />
+              <label htmlFor="pickuptime">Pickup Time (Please use 24-hour time with the xx:xx format!)</label>
+              <span className="red-text">{errors.pickuptime}</span>
+            </div>
+
+            {/* Submit Button */}
+            <button
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+                marginTop: "1rem"
+              }}
+              onClick={this.onSubmitClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Submit
+            </button>
+
+            {/* Logout Button */}
             <button
               style={{
                 width: "150px",
@@ -33,12 +135,14 @@ return (
             >
               Logout
             </button>
+
           </div>
         </div>
       </div>
     );
   }
 }
+
 Dashboard.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
