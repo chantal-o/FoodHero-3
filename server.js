7e8b92427a1d23c5f8c8f004ac713e132c273394
Rasfan M Faiz <rasfanfaiz@gmail.com>
2019-11-16 12:48:54 -0500

    maybeafix

--- server.js
+++ server.js
@@ -81,5 +81,5 @@ app.use("/api/users", users);
 
 
 
-const port = process.env.PORT || "5000"; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.mongoURI || "5000"; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
