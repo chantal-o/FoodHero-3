0e7ad6f924139906e2de4eb7122d776ff6b5bd0c
MFRasfan <rasfanfaiz@gmail.com>
2019-12-08 19:34:00 -0500

    Updates

---
+++ precache-manifest.844e4a89ae52078c0954d6ba4b195a8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bac5fd9713fe5c7ff81314933dd42c",
+    "url": "/FoodHero-3/index.html"
+  },
+  {
+    "revision": "bb502ddb05b461544527",
+    "url": "/FoodHero-3/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e3240333fe7aa9a8307f",
+    "url": "/FoodHero-3/static/js/2.797b1ef1.chunk.js"
+  },
+  {
+    "revision": "bb502ddb05b461544527",
+    "url": "/FoodHero-3/static/js/main.e2abe11c.chunk.js"
+  },
+  {
+    "revision": "374e6acdb67b8d0db016",
+    "url": "/FoodHero-3/static/js/runtime-main.3ce27f2f.js"
+  }
+]);
