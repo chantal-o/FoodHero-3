bd01416e3e7bab9f3f2000f9c1d84096444d7438
MFRasfan <rasfanfaiz@gmail.com>
2019-12-09 23:17:54 -0500

    port

--- server.js
+++ server.js
@@ -87,5 +87,5 @@ app.post('/inventory', (req, res) => {
   })
 });
 
-const port = process.env.mongoURI || "5000"; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
